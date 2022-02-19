b5d72cb6abdae2527ab2455af064ec53b3ff7281
Nay Thu Khant <naythukhant644@gmail.com>
2022-02-19 19:52:28 +0630

    bcrypt updated

--- controllers/api/auth.js
+++ controllers/api/auth.js
@@ -11,15 +11,24 @@ module.exports = {
 
         const {username, email, password} = req.body
         try {
-            const salt = await bcrypt.genSalt(10)
-            const passwordHash = await bcrypt.hash(password, salt)
+            await bcrypt.genSalt(10, async (error, salt) => {
+                if (error) {
+                    return generalHelper.handleError(error, res)
+                }
 
-            let user = await User.create({username, email, password: passwordHash})
-            const account = await accountHelper.createAnAccountForUser(user._id)
-            user = JSON.parse(JSON.stringify(user))
-            user.token = await jwtHelper.generateTokenForUser(user._id)
-            user.account = account._id
-            return responder.successResponse(user, res, "Registered successfully")
+                await bcrypt.hash(password, salt, null, async (error, hash) => {
+                    if (error) {
+                        return generalHelper.handleError(error, res)
+                    }
+
+                    let user = await User.create({username, email, password: hash})
+                    const account = await accountHelper.createAnAccountForUser(user._id)
+                    user = JSON.parse(JSON.stringify(user))
+                    user.token = await jwtHelper.generateTokenForUser(user._id)
+                    user.account = account._id
+                    return responder.successResponse(user, res, "Registered successfully")
+                })
+            })
 
         } catch (error) {
             return generalHelper.handleError(error, res)
@@ -34,11 +43,16 @@ module.exports = {
         const {email, password} = req.body
         try {
             let user = await User.findOne({email}).select('+password')
-            if (user && await bcrypt.compare(password, user.password)) {
-                user = JSON.parse(JSON.stringify(user))
-                user.token = await jwtHelper.generateTokenForUser(user._id)
-                delete user.password
-                return responder.successResponse(user, res, "Logged In successfully")
+            if (user) {
+                await bcrypt.compare(password, user.password, async (error, isCorrectPassword) => {
+                    if (!isCorrectPassword) {
+                        return responder.unauthorizedResponse(res)
+                    }
+                    user = JSON.parse(JSON.stringify(user))
+                    user.token = await jwtHelper.generateTokenForUser(user._id)
+                    delete user.password
+                    return responder.successResponse(user, res, "Logged In successfully")
+                })
             } else {
                 return responder.unauthorizedResponse(res)
             }
